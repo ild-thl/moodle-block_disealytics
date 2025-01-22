3994ad9721daa2ed0800e025ecc2568e450d96b2
Anna-Lena <anna-lena.langhans@stud.th-luebeck.de>
2025-01-22 12:08:49 +0100

    feat(statistic-insights): bump version

--- lang/en/block_disealytics.php
+++ lang/en/block_disealytics.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $string['pluginname'] = 'Learner Dashboard';
 $string['plugin-title'] = 'Learner Dashboard';
-$string['plugin-version-details'] = 'DiSEA Learner Dashboard 2024 - Version 0.3.0 2024121800';
+$string['plugin-version-details'] = 'DiSEA Learner Dashboard 2024 - Version 0.3.0 2025012200';
 $string['disea'] = 'Learner Dashboard';
 $string['disealytics:addinstance'] = 'Add a new Learner Dashboard block.';
 $string['disealytics:myaddinstance'] = 'Add a new Learner Dashboard block to my dashboard.';

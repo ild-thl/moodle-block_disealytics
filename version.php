00d36acc79318d14b86aa97a0002fff1aa3ee5eb
Anna-Lena <anna-lena.langhans@stud.th-luebeck.de>
2025-02-12 16:59:46 +0100

    feat(statistic-insights): add de fallback text for not available courses

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_disealytics';
 $plugin->release = '0.3.0';
-$plugin->version = 2025021204;
+$plugin->version = 2025021205;
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_STABLE;

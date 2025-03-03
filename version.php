61b9ac564385f2e4b511f6b6fd9dd26aff745a95
Bruhns, Jan-Marco <jan-marco.bruhns@th-luebeck.de>
2025-03-03 13:09:15 +0100

    fix(): update README.md

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_disealytics';
 $plugin->release = '0.3.0';
-$plugin->version = 2025022602;
+$plugin->version = 2025030300;
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_STABLE;

5ca01dff78f9a4d1af72a0d866b5860f78d31a1b
Jan-Marco Bruhns <jan-marco.bruhns@th-luebeck.de>
2026-03-16 13:11:07 +0100

    fix(): remove wrong langstring references

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_disealytics';
 $plugin->release = '0.3.1';
-$plugin->version = 2026032300;
+$plugin->version = 2026031600;
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_STABLE;
